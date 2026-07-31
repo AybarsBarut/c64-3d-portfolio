3b24e85b9bda48aa42e14209750bf41307febb6a
AybarsBarut <barutaybarsfahri@gmail.com>
2026-07-31 11:13:02 +0300

    Fix Commodore Datassette 3D model geometry clipping, spool reel rotation, and open/close lid animation

--- src/components/scene/CassetteTape.tsx
+++ src/components/scene/CassetteTape.tsx
@@ -12,69 +12,114 @@ export function CassetteTape() {
   };
 
   return (
-    // Rear left side desk space [-3.2, 0.45, -1.6]
+    // Rear left side desk position [-3.2, 0.45, -1.6]
     <group position={[-3.2, 0.45, -1.6]} onClick={handleClick}>
-      {/* Datasette Main Box */}
+      {/* Datasette Main Box Body (C2N Style) */}
       <mesh castShadow receiveShadow position={[0, 0, 0]}>
-        <boxGeometry args={[1.5, 0.35, 2.2]} />
-        <meshStandardMaterial color="#333333" roughness={0.5} />
+        <boxGeometry args={[1.5, 0.32, 2.2]} />
+        <meshStandardMaterial color="#3a3733" roughness={0.55} metalness={0.1} />
       </mesh>
 
-      {/* Cassette Well Recess */}
-      <mesh position={[0, 0.16, 0.2]}>
-        <boxGeometry args={[1.1, 0.08, 0.8]} />
-        <meshStandardMaterial color="#1a1a1a" roughness={0.8} />
+      {/* Top Silver Metallic Trim Frame */}
+      <mesh position={[0, 0.161, -0.2]}>
+        <boxGeometry args={[1.35, 0.005, 1.6]} />
+        <meshStandardMaterial color="#888899" roughness={0.3} metalness={0.6} />
       </mesh>
 
-      {/* Cassette Tape Model (Visible inside well) */}
-      <group position={[0, 0.19, 0.2]} rotation={[-0.1, 0, 0]}>
+      {/* Recessed Cassette Well (Sunken into body) */}
+      <mesh position={[0, 0.10, 0.2]}>
+        <boxGeometry args={[1.15, 0.12, 0.85]} />
+        <meshStandardMaterial color="#141414" roughness={0.9} />
+      </mesh>
+
+      {/* Cassette Tape Model */}
+      <group
+        position={[0, cassetteInserted ? 0.22 : 0.11, cassetteInserted ? 0.10 : 0.2]}
+        rotation={[cassetteInserted ? -0.35 : 0, 0, 0]}
+      >
         {/* Cassette Shell */}
         <mesh castShadow position={[0, 0, 0]}>
-          <boxGeometry args={[1.0, 0.06, 0.65]} />
-          <meshStandardMaterial color="#222222" roughness={0.3} />
+          <boxGeometry args={[1.0, 0.05, 0.65]} />
+          <meshStandardMaterial color="#222226" roughness={0.35} />
         </mesh>
 
-        {/* Cassette Red/White Label */}
-        <mesh position={[0, 0.032, -0.05]}>
-          <boxGeometry args={[0.88, 0.002, 0.35]} />
-          <meshStandardMaterial color="#e53e3e" roughness={0.4} />
+        {/* Cassette Red/Orange Retro Label */}
+        <mesh position={[0, 0.026, -0.05]}>
+          <boxGeometry args={[0.88, 0.001, 0.35]} />
+          <meshStandardMaterial color="#dc2626" roughness={0.4} />
         </mesh>
 
-        {/* Tape Reels (Left & Right Spool Holes) */}
+        {/* Inner Label White Stripe */}
+        <mesh position={[0, 0.027, -0.05]}>
+          <boxGeometry args={[0.80, 0.001, 0.12]} />
+          <meshStandardMaterial color="#f8fafc" roughness={0.3} />
+        </mesh>
+
+        {/* Tape Reels (Flat Ring Spool Discs, facing UP) */}
         {[-0.22, 0.22].map((xPos, idx) => (
-          <mesh key={idx} position={[xPos, 0.033, -0.05]} rotation={[Math.PI / 2, 0, 0]}>
-            <cylinderGeometry args={[0.08, 0.08, 0.005, 16]} />
-            <meshStandardMaterial color="#ffffff" roughness={0.2} />
+          <mesh key={idx} position={[xPos, 0.028, -0.05]} rotation={[0, 0, 0]}>
+            <cylinderGeometry args={[0.08, 0.08, 0.002, 24]} />
+            <meshStandardMaterial color="#e2e8f0" roughness={0.2} />
           </mesh>
         ))}
 
-        {/* Magnetic Tape Ribbon Window */}
-        <mesh position={[0, 0, 0.25]}>
-          <boxGeometry args={[0.7, 0.05, 0.12]} />
-          <meshStandardMaterial color="#3d2314" roughness={0.6} />
+        {/* Reel Hub Center Holes */}
+        {[-0.22, 0.22].map((xPos, idx) => (
+          <mesh key={idx} position={[xPos, 0.029, -0.05]}>
+            <cylinderGeometry args={[0.035, 0.035, 0.003, 16]} />
+            <meshStandardMaterial color="#0f172a" roughness={0.9} />
+          </mesh>
+        ))}
+
+        {/* Magnetic Tape Ribbon Window at bottom edge */}
+        <mesh position={[0, 0, 0.27]}>
+          <boxGeometry args={[0.72, 0.045, 0.10]} />
+          <meshStandardMaterial color="#2e1005" roughness={0.5} />
         </mesh>
       </group>
 
-      {/* Transparent Plastic Lid (Pivoted smoothly at rear edge hinge [0, 0.18, -0.21]) */}
-      <group position={[0, 0.18, -0.21]} rotation={[cassetteInserted ? -0.65 : 0, 0, 0]}>
-        <mesh position={[0, 0.02, 0.41]} castShadow>
-          <boxGeometry args={[1.12, 0.02, 0.82]} />
-          <meshStandardMaterial color="#77aaff" transparent opacity={0.35} roughness={0.1} />
+      {/* Transparent Smoked Glass Lid (Hinged at rear [0, 0.165, -0.22]) */}
+      <group position={[0, 0.165, -0.22]} rotation={[cassetteInserted ? -0.75 : 0, 0, 0]}>
+        <mesh position={[0, 0.01, 0.42]} castShadow>
+          <boxGeometry args={[1.14, 0.015, 0.84]} />
+          <meshStandardMaterial
+            color="#64748b"
+            transparent
+            opacity={0.45}
+            roughness={0.1}
+            metalness={0.2}
+          />
+        </mesh>
+        {/* Plastic Lid Handle Ridge */}
+        <mesh position={[0, 0.02, 0.82]}>
+          <boxGeometry args={[0.3, 0.015, 0.04]} />
+          <meshStandardMaterial color="#475569" roughness={0.3} />
         </mesh>
       </group>
 
-      {/* Control Buttons (Stop, Play, Record, Rewind, FF, Eject) */}
-      {[-0.5, -0.3, -0.1, 0.1, 0.3, 0.5].map((x, i) => (
-        <mesh key={i} position={[x, 0.18, -0.8]}>
-          <boxGeometry args={[0.15, 0.12, 0.22]} />
-          <meshStandardMaterial color={i === 2 ? '#cc2222' : '#555555'} roughness={0.4} />
-        </mesh>
-      ))}
+      {/* Datassette Control Buttons (RECORD [Red], PLAY, REW, FF, STOP, EJECT) */}
+      {[-0.5, -0.3, -0.1, 0.1, 0.3, 0.5].map((x, i) => {
+        const isPressed = (i === 1 && !cassetteInserted) || (i === 5 && cassetteInserted);
+        return (
+          <mesh key={i} position={[x, isPressed ? 0.14 : 0.17, -0.78]} castShadow>
+            <boxGeometry args={[0.16, 0.12, 0.24]} />
+            <meshStandardMaterial
+              color={i === 0 ? '#ef4444' : '#475569'}
+              roughness={0.4}
+              metalness={0.1}
+            />
+          </mesh>
+        );
+      })}
 
-      {/* Counter Display */}
-      <mesh position={[0.4, 0.18, -0.3]}>
-        <boxGeometry args={[0.25, 0.04, 0.15]} />
-        <meshStandardMaterial color="#050505" />
+      {/* Digital Counter Display */}
+      <mesh position={[0.42, 0.17, -0.3]}>
+        <boxGeometry args={[0.26, 0.03, 0.16]} />
+        <meshStandardMaterial color="#020617" roughness={0.2} />
+      </mesh>
+      <mesh position={[0.42, 0.186, -0.3]}>
+        <boxGeometry args={[0.20, 0.001, 0.10]} />
+        <meshStandardMaterial color="#22c55e" emissive="#22c55e" emissiveIntensity={0.8} />
       </mesh>
     </group>
   );
